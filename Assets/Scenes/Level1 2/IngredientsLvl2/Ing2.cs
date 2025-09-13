ffd3b52652d8ac85af5d747a266587861607b23f
Rushi57 <rushihanzo07@gmail.com>
2025-09-13 22:09:00 +0800

    D
    Some minor

---
+++ Assets/Scenes/Level1 2/IngredientsLvl2/Ing2.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ing2", menuName = "CookingGame/Ing2")]
+public class Ing2 : ScriptableObject
+{
+    public string ingredientsName2;
+    public Sprite ingredientsSprite2;
+    public GameObject ingredientsPrefab2;
+}
--- Assets/Scenes/Level1/DropZone.cs
+++ Assets/Scenes/Level1/DropZone.cs
@@ -7,8 +7,17 @@ public class DropZone : MonoBehaviour, IDropHandler
     {
         if (eventData.pointerDrag != null)
         {
-            // Snap the ingredient to this drop zone
-            eventData.pointerDrag.GetComponent<RectTransform>().position = transform.position;
+            RectTransform draggedObject = eventData.pointerDrag.GetComponent<RectTransform>();
+
+            // Set this DropZone as the parent of the dragged object
+            draggedObject.SetParent(transform);
+
+            // Reset local position to center it
+            draggedObject.localPosition = Vector3.zero;
+
+            // (Optional) reset local scale and rotation to match the drop zone
+            draggedObject.localRotation = Quaternion.identity;
+            draggedObject.localScale = Vector3.one;
         }
     }
 }
